57ac9f1d9b07114b2e17d1d5d3297ba5cac16724
James58899 <james58899@users.noreply.github.com>
2019-01-20 10:02:44 +0800

    try simplify script

--- control_files/control.sh
+++ control_files/control.sh
@@ -1,36 +1,20 @@
-runResult=""
-runExitCode=0
-
 function sendRequest(){
-    local command="$@"
-    runResult=`./mcrcon -H 127.0.0.1 -P 23456 "$command"`
-    runExitCode=$?
+    ./mcrcon -H 127.0.0.1 -P 23456 $@
 }
 
 function ping(){
     sendRequest "sponge version"
-    if [[ runExitCode == 0 ]] ; then
-        echo 0
-        runExitCode=0
-    else
-        echo 1
-        runExitCode=1
-    fi
 }
 
 function stop(){
     sendRequest "stop"
-    if [[ runExitCode == 0 ]] ; then
-        waitingStop=1
-        until [ waitingStop == 0 ]
-        do
-            if $(ping) == 0 ; then
-                waitingStop=0
-            fi
-            sleep 1
-        done
-        runExitCode=0
-    else
-        runExitCode=1
-    fi
 }
+
+case $1 in
+    ping)
+        ping
+        ;;
+    stop)
+        stop
+        ;;
+esac
