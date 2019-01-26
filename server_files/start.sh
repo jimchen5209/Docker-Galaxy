cbf3773b77d19bf46f249471aec8e60a3050dc62
sciuridae0603 <sciuridae0603@gmail.com>
2019-01-26 23:13:11 +0800

    add auto remove empty level dat file

--- server_files/start.sh
+++ server_files/start.sh
@@ -1,2 +1,3 @@
 cd /app/minecraft/
+find . -iname "level*.dat" -type f -empty -delete
 java -Xmx$MEMORY_MAX -Xtune:virtualized -Xshareclasses:name=minecraft,cacheDir=/app/javasharedresources,nonfatal -jar forge-$FORGE_VERSION-universal.jar
