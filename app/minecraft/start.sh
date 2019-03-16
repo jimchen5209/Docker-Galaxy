48f271f8ba72b67e36d0188421819512475bf7bb
james58899 <james59988@gmail.com>
2019-03-16 09:44:31 +0800

    Remove unnecessary setting fix script not working on alpine

--- app/minecraft/start.sh
+++ app/minecraft/start.sh
@@ -1,3 +1,2 @@
-cd /app/minecraft/
 find . -iname "level*.dat" -type f -size 0 -delete
 java -Xmx$MEMORY_MAX -Xtune:virtualized -Xshareclasses:name=minecraft,cacheDir=/app/javasharedresources,nonfatal -jar forge-$FORGE_VERSION-universal.jar
